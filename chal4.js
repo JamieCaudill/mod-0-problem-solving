d679bdda24f817dcd866382c2a7e54ff4d91ef3a
Jamie Caudill <j.caudill7177@gmail.com>
2023-03-03 11:33:31 -0800

    Write pseudocode and create chal4 file

---
+++ chal4.js
@@ -0,0 +1,18 @@
+// Challenge 4
+
+// Write code that prints only the words in an array that include the string "ing"
+
+// Create an array of activities including words that include the string "ing"
+var activities = ["climbing", "surf", "skiing", "run"];
+
+
+// Write a for loop that iterates through each element in the activities array
+for (var i = 0; i < activities.length; i++) {
+    // Create a variable that represents each element individually
+    var activity = activities[i];
+    // Write a conditional that checks each element for the string "ing"
+    if (activity.includes("ing")) {
+        // Print the elements that include "ing"
+        console.log(activity);
+    }
+}
--- problems.js
+++ problems.js
@@ -1,20 +1,11 @@
-// Challenge 4
-
-var verbs = ["bowling", "swimming", "fishing", "read"]
-
-for (var i = 0; i < verbs.length; i++) {
-    var words = verbs[i];
-    if (words.includes("ing")) {
-        console.log(words);
-    }
-}
 
 // Challenge 5
 
-var travel = ["Italy", "Brazil", "Colombia", "Aspen"];
-var place = travel.sort();
-console.log(place)
+//
 
-for (var i = 0; i < place.length; i++) {
-    console.log(`I'm going to ${place[i]}`);
+var destination = ["Peru", "Sri Lanka", "Haiti", "Timbuktu"];
+var travel = destination.sort();
+
+for (var i = 0; i < travel.length; i++) {
+    console.log(`I'm going to ${travel[i]}`);
 }
